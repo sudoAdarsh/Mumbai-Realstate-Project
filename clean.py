6d9405c938e6d1965ebb18b33335d5dc5422297f
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2026-06-10 14:31:59 +0530

    Added cleaned dataset for model training

--- clean.py
+++ clean.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 df = pd.read_csv("mumbai_real_estate_dataset.csv")
 
-
+# POSSESION
 def map_possession(val):
     if not isinstance(val, str):
         return 0  # Handle NaNs/non-strings as 0 (Everything else)
@@ -16,7 +16,11 @@ def map_possession(val):
     else:
         return 0
 
+df["possesion_by"] = df["possesion_by"].apply(map_possession)
 
+
+
+# TYPE
 residential = [
     "Apartment",
     "Builder Floor Apartment",
@@ -43,7 +47,6 @@ industrial = [
 ]
 
 
-# Function to map type
 def map_type(val):
     if val in residential:
         return 1
@@ -54,19 +57,80 @@ def map_type(val):
     else:
         return 0
 
-
-furnished_mapping = {"Unfurnished": 0, "Semi-Furnished": 0.5, "Furnished": 1}
-
-
-df["transaction"] = (
-    df["transaction"].replace({"Rent": "Resale", "Other": "Resale"}).fillna("Resale")
-)
-
-df["possesion_by"] = df["possesion_by"].apply(map_possession)
-
-
 df["type"] = df["type"].apply(map_type)
 
 
-# print(df['type'].unique())
+
+# FURNISHED
+furnished_mapping = {"Unfurnished": 0, "Semi-Furnished": 0.5, "Furnished": 1}
+
+df['furnished'] = df['furnished'].map(furnished_mapping).fillna(0)
+
+
+
+# TRANSACTION
+df["transaction"] = (
+    df["transaction"].replace({"Rent": "Resale", "Other": "Resale"}).fillna("Resale")
+
+)
+
+df['transaction'] = df["transaction"].map({"Resale": 0, "New Property": 1})
+
+
+
+# PRICE
+lower_limit = 1300000
+upper_limit = 200000000
+
+df = df[(df['price'] >= lower_limit) & (df['price'] <= upper_limit)].copy()
+
+
+# BEDROOM & BATHROOM
+df['bedrooms'] = pd.to_numeric(df['bedrooms'], errors='coerce')
+df['bathrooms'] = pd.to_numeric(df['bathrooms'], errors='coerce')
+
+df['area_bucket'] = pd.cut(df['area'], bins=[0, 500, 1000, 2000, 50000], labels=[0, 1, 2, 3])
+bedroom_medians = df.groupby(['area_bucket', 'type'])['bedrooms'].transform('median')
+bathroom_medians = df.groupby(['area_bucket', 'type'])['bathrooms'].transform('median')
+
+
+df['bedrooms'] = df['bedrooms'].fillna(bedroom_medians)
+df['bathrooms'] = df['bathrooms'].fillna(bathroom_medians)
+
+df['bedrooms'] = df['bedrooms'].fillna(df['bedrooms'].median())
+df['bathrooms'] = df['bathrooms'].fillna(df['bathrooms'].median())
+df = df.drop(columns="area_bucket")
+
+
+
+# PARKING
+df = df.drop(columns='parking',)
+
+
+# LAT & LONG
+df = df.dropna(subset=['lat', 'long'])
+
+
+# ENCODING CITY, LOCALITY, PROJECT NAME, DEVELOPER
+cols_to_encode = ['project_name', 'locality', 'city', 'developers']
+
+for col in cols_to_encode:
+    target_mapping = df.groupby(col)['price'].mean()
+    df[f'{col}_encoded'] = df[col].map(target_mapping)
+
+
+# MISSING AREA
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+
+imputer = IterativeImputer(random_state=42)
+
+numeric_cols = df.select_dtypes(include=['number']).columns
+
+df[numeric_cols] = imputer.fit_transform(df[numeric_cols])
+
+df.to_csv("mumbai_real_estate_cleaned.csv", index=False)
+
+print(df.info())
 print(df.isnull().sum())
+print(len(df))
